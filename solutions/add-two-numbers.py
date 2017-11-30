b84baef9d4b889b3594da1f1323f7782968532aa
Yufei Liu <yufeiliu94@gmail.com>
2017-11-30 21:18:52 +0800

    :memo: 2. Add Two Numbers

--- solutions/add-two-numbers.py
+++ solutions/add-two-numbers.py
@@ -1,12 +1,16 @@
 # encoding: UTF-8
 
 from leetcode import *
+from typing import Generator
 
 
 @Problem(2, 'Add Two Numbers', Difficulty.Medium, Tags.LinkedList, Tags.Math)
 class Solution:
     @staticmethod
-    def iterate(xs: ListNode, ys: ListNode):
+    def iterate(xs: ListNode, ys: ListNode) -> Generator[int, None, None]:
+        """
+        模拟十进制加法, 从低到高逐位返回各个数位
+        """
         carry, val = 0, 0
         while xs is not None and ys is not None:
             carry, val = divmod(xs.val + ys.val + carry, 10)
@@ -24,6 +28,9 @@ class Solution:
             yield carry
 
     def addTwoNumbers(self, lhs: ListNode, rhs: ListNode) -> ListNode:
+        """
+        从十进制加法生成的数位中构建新的链表
+        """
         it = self.iterate(lhs, rhs)
         result = ListNode(next(it))
         cursor = result
@@ -34,5 +41,5 @@ class Solution:
 
 
 @Solution.test.addTwoNumbers
-def example(fn):
+def examples(fn):
     require(fn(ListNode.from_list([2, 4, 3]), ListNode.from_list([5, 6, 4])) == ListNode.from_list([7, 0, 8]))
