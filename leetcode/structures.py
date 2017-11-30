da4535d3e13041d5500b03d52bc098de576d3e9f
YufeiLiu <yufeiliu94@gmail.com>
2017-12-01 01:50:24 +0800

    :beers: 添加二叉树支持

--- leetcode/structures.py
+++ leetcode/structures.py
@@ -1,5 +1,7 @@
 # encoding: UTF-8
 
+import enum
+
 
 class ListNode:
     def __init__(self, x):
@@ -33,3 +35,71 @@ class ListNode:
 
     def __str__(self):
         return '({})'.format(' -> '.join(self.to_list()))
+
+
+class TreeBuildState(enum.Enum):
+    Left, Right = 'Left', 'Right'
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    @property
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
+    @staticmethod
+    def from_list(xs):
+        ts, root = [], None
+        try:
+            for val in xs:
+                if not ts:
+                    root = TreeNode(val)
+                    ts.append((root, TreeBuildState.Right))
+                    ts.append((root, TreeBuildState.Left))
+                else:
+                    node, tbs = ts.pop()
+                    if val is not None:
+                        if tbs is TreeBuildState.Left:
+                            node.left = TreeNode(val)
+                            ts.append((node, TreeBuildState.Right))
+                            ts.append((node.left, TreeBuildState.Right))
+                            ts.append((node.left, TreeBuildState.Left))
+                        else:
+                            node.right = TreeNode(val)
+                            ts.append((node.right, TreeBuildState.Right))
+                            ts.append((node.right, TreeBuildState.Left))
+
+            return root
+        except:
+            raise ValueError('Illegal tree {!r}'.format(xs))
+
+    def to_list(self, out=None, index=1) -> [int]:
+        if out is None:
+            out = []
+        out.append(self.val)
+        if not self.is_leaf:
+            if self.left is not None:
+                self.left.to_list(out=out, index=index * 2)
+            else:
+                out.append(None)
+            if self.right is not None:
+                self.right.to_list(out=out, index=index * 2 + 1)
+            else:
+                out.append(None)
+        if index == 1:
+            while out[-1] is None:
+                out.pop()
+        return out
+
+    def __eq__(self, other):
+        return isinstance(other, TreeNode) and self.to_list() == other.to_list()
+
+    def __repr__(self):
+        return 'TreeNode.from_list({!r})'.format(self.to_list())
+
+    def __str__(self):
+        return repr(self.to_list())
